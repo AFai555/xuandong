b7c69bb3a70b83eb547ba65cf8b55f6826e65d06
Abel <Abel_676@163.com>
2018-02-06 22:24:40 +0800

    能跑起来

--- include/common.inc.php
+++ include/common.inc.php
@@ -1,7 +1,7 @@
 <?php
 //数据库连接参数
 define('DB_HOST','localhost');
-define('DB_USER','xuandong123');
-define('DB_PWD','TrzADwcc5A');
+define('DB_USER','root');
+define('DB_PWD','');
 define('DB_NAME','xuandong123');
 ?>
