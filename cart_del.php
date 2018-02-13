6085370ee88e1cc8a9f1eefaf7a5e5e977cce63c
Alvin <543046534@qq.com>
2018-02-13 22:46:38 +0800

    修复删除购物车时价格异常的问题

--- cart_del.php
+++ cart_del.php
@@ -1,6 +1,6 @@
 <?php
 require 'session2.php';
-
+$vip=_mysql_show("SELECT * FROM vip WHERE id= 1");
 //加入购物车
 $arr['s']=0;
 
@@ -17,11 +17,22 @@ $_result=_query("SELECT * FROM cart WHERE uid={$_SESSION['userid']} AND zt=0");
 $z_price=0;
 $list=array();
 while (!!$row=_mysql_list($_result)) {
-	$z_price+=$row['price'];
+	if ($_SESSION['userid']) {
+	 	if($vip['kd']=='1') { 
+			if($_SESSION['user_grade']=="钻石会员") {
+				$a['price']=$row['price']+$vip['lv3'];				
+			} else if($_SESSION['user_grade']=="高级会员") {
+				$a['price']=$row['price']+$vip['lv2'];
+			} else {
+				$a['price']=$row['price']+$vip['lv1'];
+			}
+		}
+	}	
+	
 	
 	$a['name']=getDbName('meiti_case','title',$row['pid']);
-	$a['price']=$row['price'];
 	$a['id']=$row['pid'];
+	$z_price+=$a['price'];
 	$list[]=$a;
 }
 $arr['s']=1;
