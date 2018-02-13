3f278af944a1a842b85503bffe1d7da458f9d24b
Alvin <543046534@qq.com>
2018-02-13 12:15:44 +0800

    修改数据库配置

--- include/common.inc.php
+++ include/common.inc.php
@@ -1,7 +1,7 @@
 <?php
 //数据库连接参数
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PWD','');
-define('DB_NAME','xuandong123');
+define('DB_HOST','120.78.131.21');
+define('DB_USER','xuandong');
+define('DB_PWD','candy.123');
+define('DB_NAME','xuandong');
 ?>
